c0f2832fcd9b9c6cc97241489d2063e6404afc78
Antonio Vitor <64814651+T8ninho@users.noreply.github.com>
2023-01-15 12:47:44 -0300

    master

--- src/Files/Components/Informacoes.js
+++ src/Files/Components/Informacoes.js
@@ -7,20 +7,10 @@ export default function Informacoes(){
                 <h2>Um pouco sobre mim</h2>
                 <h1> </h1>
                 <div class="about--section">
-                    <div 
-                        class="profile-picture"
-                        data-aos="fade-right"
-                        data-aos-offset="300"
-                        data-aos-duration="1500"
-                    >
+                    <div class="profile-picture">
                         <img src={Perfil} alt=""/>
                     </div>
-                    <div 
-                        class="about--text"
-                        data-aos="fade-down"
-                        data-aos-delay="300"
-                        data-aos-duration="2000"
-                    >
+                    <div class="about--text">
                         <h3>Olá, tudo bom?</h3>
                         <p>Eu sou Antonio Vitor, tenho 22 anos e tenho uma grande paixão por desenvolvimento de sites. Comecei na área apenas com <span>HTML</span> e <span>CSS</span>, atualmente estou estudando <span>JavaScript</span> combinado com <span>ReactJS</span> para tornar ainda mais atrativas minhas criações. Nesta página estão alguns dos meus projetos, fique a vontade para
                             visitar cada um deles.</p>
