bc63673700aedb2bff8a31d0bc4d6fe1e35870a2
T8ninho <tonisantos.contato@gmail.com>
2024-06-24 09:49:54 -0300

    add: Adicionado projeto novo e corrigida página de erro

--- src/index.js
+++ src/index.js
@@ -6,23 +6,6 @@ import Portifolio from './Files/Pages/Portifolio';
 import GitHubRepos from './Files/Pages/GitHubRepos';
 import PaginaErro from './PaginaErro';
 
-const ErroPage = () => {
-  return(
-    <div style={{
-        display: 'flex', 
-        alignItems: 'center', 
-        justifyContent: 'center', 
-        height: '100vh', 
-        flexDirection: 'column', 
-        gap: 15, fontSize: '150%'
-      }}>
-      <h1>Erro: 404</h1>
-      <hr />
-      <p>Ixi parece que essa tela não existe</p>
-    </div>
-  )
-}
-
 const router = createBrowserRouter([
   {
     path: "/",
