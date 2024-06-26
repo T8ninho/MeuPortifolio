3271d0dcfd3b5abf5144a15b6cd6905b8206e397
T8ninho <tonisantos.contato@gmail.com>
2024-06-25 21:55:50 -0300

    add ✅: deploy via vercel.com

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 import Portifolio from './Files/Pages/Portifolio';
 import GitHubRepos from './Files/Pages/GitHubRepos';
@@ -22,5 +23,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    <SpeedInsights />
   </React.StrictMode>
 );
